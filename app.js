db3eb190217e162984c6be3930f3559eac62590c
Yiwen789 <luoy2@Carleton.edu>
2022-04-20 12:42:36 -0400

    added strTimes in courseSchema model

--- app.js
+++ app.js
@@ -375,7 +375,7 @@ app.use(function(err, req, res, next) {
 //  Starting up the server!
 // *********************************************************** //
 //Here we set the port to use between 1024 and 65535  (2^16-1)
-const port = "5000";
+const port = "5050";
 app.set("port", port);
 
 // and now we startup the server listening on that port
